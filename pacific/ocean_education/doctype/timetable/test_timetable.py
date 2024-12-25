4ad4b08b07698e9b0c961687b0c09ab5dbddbd2f
santosh313 <santoshsutar3130@gmail.com>
2024-12-25 20:11:50 +0530

    Added ocean education module and timetable doctype

---
+++ pacific/ocean_education/doctype/timetable/test_timetable.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, santoshsutar3130@gmail.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTimetable(FrappeTestCase):
+	pass
